c767b7ece71962bfc6be148d11a7227d13429b4a
RodrigoTrevmore <sosimousa@gmail.com>
2025-08-02 02:04:29 -0400

    final update

--- server.py
+++ server.py
@@ -7,16 +7,26 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
+
 def sent_emotion_detector():
+    """
+    Run the analysis and return the response
+    and handle the errors
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    if response['dominant_emotion'] != None :
+    if response['dominant_emotion'] is not None :
         return f"For the given statement, the system response is {response}"
-    elif response['dominant_emotion'] == None :
+    if response['dominant_emotion'] is None :
         return "Invalid text! Please try again"
+
+    return None
 
 @app.route("/")
 def render_index_page():
+    """
+    Render the HTML Page
+    """
     return render_template('index.html')
 
 
